83fcc45df89f0aacb8347d2fab34b0fc3f6ee7d0
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-06-18 03:14:39 +0100

    Rest Countries API completed

--- country.js
+++ country.js
@@ -1,6 +1,86 @@
-const countrydata=new URLSearchParams(window.location.search).get('name');
-console.log(countrydata);
+const countrydata=new URLSearchParams(location.search).get('name');
+const backButton=document.querySelector('.back-button');
+const themeChanger=document.querySelector('.mode');
+const iconChanger=document.querySelector('.mode i');
+const modeWord=document.querySelector('#mode-word');
 
+console.log(countrydata);
+const countryDetails=document.querySelector('.country-details');
 fetch(`https://restcountries.com/v3.1/name/${countrydata}?fullText=true`)
 .then((response)=>response.json())
-.then((data)=>console.log(data))
+.then((data)=>{
+   
+//   console.log(data)
+    countryDetails.innerHTML=`
+                <img src="${data[0].flags.svg}" alt="${data[0].flags.alt}">
+                <div class="details-text-container">
+                    <h1>${data[0].name.common}</h1>
+                    <div class="details-text">
+                        <p><b>Native Name: </b>${Object.values(data[0].name.nativeName)[0].official}</p>
+                        <p><b>Region: </b>${data[0].region}</p>
+                        <p><b>Population: </b>${data[0].population.toLocaleString('en-IN')}</p>
+                        <p><b>Sub Region: </b>${data[0].subregion}</p>
+                        <p><b>Capital: </b>${data[0].capital}</p>
+                        <p><b>Top Level Domain: </b>${data[0].tld.join(", ")}</p>
+                        <p><b>Currencies: </b>${Object.values(data[0].currencies)[0].name}</p>
+                        <p><b>Languages: </b>${Object.keys(data[0].languages).join(", ")}</p>
+    
+                    </div>
+                    <div class="border-content">
+                        <b>Border Countries:</b>
+    
+                    </div>
+                </div>
+            `
+            const borderContent=document.querySelector('.border-content');
+            const message='No border countries. '
+            if(data[0].borders){
+                data[0].borders.forEach((border) => {
+                    fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                    .then((res)=>res.json())
+                    .then(([borderData])=>{
+                        const borderTag=document.createElement('a');
+                        borderTag.href=`/country.html?name=${borderData.name.common}`
+                        borderTag.innerHTML=`${borderData.name.common}`;
+                        borderContent.append(borderTag)
+                    });
+                });
+                
+            }
+            else{
+            borderContent.append(message);
+            }
+})
+
+backButton.addEventListener('click',()=>{
+   history.back();
+})
+
+let themeValue=localStorage.getItem('theme')==='true';
+console.log(themeValue);
+if(themeValue){
+  document.body.classList.remove('dark');
+  iconChanger.classList.add('fa-moon');
+  modeWord.innerText = "Dark";
+}
+else {
+  document.body.classList.add('dark');
+  iconChanger.classList.remove('fa-moon');
+  iconChanger.classList.add('fa-sun');
+  modeWord.innerText = "Light";
+}
+themeChanger.addEventListener('click',()=>{
+    themeValue=!themeValue;
+    localStorage.setItem('theme',themeValue);
+    document.body.classList.toggle('dark');
+    iconChanger.classList.toggle('fa-moon');
+    iconChanger.classList.toggle('fa-sun');
+ 
+    if(modeWord.innerText == "Dark"){
+     modeWord.innerText="Light ";
+    }
+    else if(modeWord.innerText == "Light"){
+     modeWord.innerText="Dark ";
+    }
+})
+// &nbsp;<a href="#">${data[0].borders[0]}</a><a href="#">Germany</a>
--- script.js
+++ script.js
@@ -1,24 +1,85 @@
 const countriesContainer=document.querySelector('.countries-container');
+const filterByRegion=document.querySelector('#select');
+const inputedValue=document.querySelector('#inpt');
+const themeChanger=document.querySelector('.mode');
+const iconChanger=document.querySelector('.mode i');
+const modeWord=document.querySelector('#mode-word');
 
+// location.reload();
+let allDatas;
 fetch('https://restcountries.com/v3.1/all')
 .then((response)=>response.json())
-.then((datas)=>{
-    console.log(datas);
-    datas.forEach((country) => {
-      const countryCard=document.createElement('a');
-      countryCard.classList.add("country-card");
-      countryCard.href=`country.html?name=${country.name.common}`
-      countryCard.innerHTML=` <img src="${country.flags.svg}" alt="flag">
-                  <div class="card-text">
-                    <h3 class="card-title">${country.name.common}</h3>
-                    <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
-                    <p><b>Region: </b>${country.region}</p>
-                    <p><b>Capital: </b>${country.capital?.[0]}</p>
-                  </div>
-                 `;
-      countriesContainer.append(countryCard); 
-    });
+.then((data)=>{
+  allDatas=data;
+  countriesAppender(data);
 });
+
+filterByRegion.addEventListener('change',(e)=>{
+   fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+   .then((res)=>res.json())
+   .then((data)=>{
+    countriesAppender(data);
+   })
+})
+
+inputedValue.addEventListener('input',(e)=>{
+   const filteredCountries=allDatas.filter((data)=>data.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+    countriesAppender(filteredCountries);   
+})
+
+if (localStorage.getItem('theme') === null){
+  
+  localStorage.setItem('theme','true');
+}
+let themeValue=localStorage.getItem('theme')==='true';
+if(themeValue){
+  document.body.classList.remove('dark');
+  iconChanger.classList.add('fa-moon');
+  modeWord.innerText = "Dark";
+}
+else {
+  document.body.classList.add('dark');
+  iconChanger.classList.remove('fa-moon');
+  iconChanger.classList.add('fa-sun');
+  modeWord.innerText = "Light";
+}
+themeChanger.addEventListener('click',()=>{
+      themeValue= !themeValue;
+    localStorage.setItem('theme',themeValue)
+   document.body.classList.toggle('dark');
+   iconChanger.classList.toggle('fa-moon');
+   iconChanger.classList.toggle('fa-sun');
+
+   if(modeWord.innerText == "Dark"){
+    modeWord.innerText="Light ";
+   }
+   else if(modeWord.innerText == "Light"){
+    modeWord.innerText="Dark ";
+   }
+  
+
+})
+
+
+function countriesAppender(data){
+  countriesContainer.innerHTML=''
+  data.forEach((country) => {
+    const countryCard=document.createElement('a');
+    countryCard.classList.add("country-card");
+    countryCard.href=`country.html?name=${country.name.common}`
+    countryCard.innerHTML=` <img src="${country.flags.svg}" alt="${country.name.common}">
+                <div class="card-text">
+                  <h3 class="card-title">${country.name.common}</h3>
+                  <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
+                  <p><b>Region: </b>${country.region}</p>
+                  <p><b>Capital: </b>${country.capital?.[0]}</p>
+                </div>
+               `;
+              //  console.log(countryCard);
+    countriesContainer.append(countryCard); 
+  });
+}
+
 function tediousTask(){
     const final=document.querySelector('.hero-section');
 
@@ -75,8 +136,7 @@ final.append(countryCard)
 
 }
 
+
 // console.log(countryCard);
 
 /* When you log countryCard to the console before setting its href and innerHTML, the console logs a reference to the countryCard element. If you then change the properties of countryCard after the console.log, the console will still display the current state of the element because it reflects the live state of the DOM element. */
-
-var a =111111111;
